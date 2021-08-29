60735fdebda5a74f998b8321d75e2b6f5df58337
Madhav Parikh <parikh.madhav1999@gmail.com>
2021-08-29 17:02:53 +0530

    authentication completed

--- app/controller/auth.py
+++ app/controller/auth.py
@@ -1,6 +1,4 @@
-from flask import render_template, Blueprint, request, session
-from datetime import timedelta, datetime
-from .models import db, Users, Creds
+from flask import render_template, Blueprint, request, session, redirect, url_for
 from .auth_controller import Auth_Verification
 
 auth = Blueprint('auth', __name__, template_folder='templates', static_folder='static',
@@ -14,18 +12,31 @@ def login():
 
 @auth.route('/otp', methods=['POST'])
 def get_otp():
-    data = Auth_Verification.user_check(request.form['email'])
-    print(data)
-    if data['Status'] == 'Success':
+    if 'librarian' in request.form:
+        data = Auth_Verification.user_check(request.form['email'], user_type='librarian')
+    elif 'user' in request.form:
+        data = Auth_Verification.user_check(request.form['email'], user_type='user')
+
+    if data['Status'] == 'Success' and session['user_type'] == "librarian":
         return render_template('auth/otp.html')
+    elif data['Status'] == 'Success' and session['user_type'] == "user":
+        return redirect(url_for('user.user_index'))
     else:
-        return "INVALID1"
+        return "INVALID"
 
 
 @auth.route('/validate/otp', methods=['POST'])
 def validate_otp():
     data = Auth_Verification.otp_check(emailid=session.get('emailid'), otp=request.form['otp'])
     if data['Status'] == 'Success':
-        "return render_template('auth/otp.html')"
+        return redirect(url_for('admin.index'))
     else:
-        return "INVALID2"
+        return "OTP Invalid"
+
+
+@auth.route('/logout')
+def logout():
+    session.pop('emailid', None)
+    session.pop('user_type', None)
+    session.clear()
+    return redirect(url_for('auth.login'))
--- app/controller/auth_controller.py
+++ app/controller/auth_controller.py
@@ -1,6 +1,5 @@
 from flask import session
 from .models import db, Users, Creds
-from flask_sqlalchemy import get_debug_queries
 from datetime import datetime
 import random
 import string
@@ -12,7 +11,7 @@ class Auth_Verification:
         letters = string.digits
         return ''.join(random.choice(letters) for i in range(length))
 
-    def user_check(emailid):
+    def user_check(emailid, user_type):
         try:
             data_exist = db.session.query(Users).filter_by(email=emailid).first()
             if not data_exist:
@@ -26,33 +25,32 @@ class Auth_Verification:
             else:
                 uid = data_exist.uid
 
-            otp = int(Auth_Verification.password_generator(6))
-            new_cred = Creds(
-                uid=uid,
-                otp=otp,
-                timestamp=datetime.now()
-            )
-            db.session.add(new_cred)
-            db.session.commit()
-            deets = {'Emailid': emailid, 'Subject': 'OTP', 'OTP': otp}
-            send_mail(**deets)
+            if user_type != 'user':
+                otp = int(Auth_Verification.password_generator(6))
+                new_cred = Creds(
+                    uid=uid,
+                    otp=otp,
+                    timestamp=datetime.now()
+                )
+                db.session.add(new_cred)
+                db.session.commit()
+
+                deets = {'Emailid': emailid, 'Subject': 'OTP', 'OTP': otp}
+                send_mail(**deets)
+
             session['emailid'] = emailid
+            session['user_type'] = user_type
+
             return {'Status': "Success"}
         except Exception as e:
             return {'Status': "Error - " + str(e)}
 
     def otp_check(emailid, otp):
         try:
-            data_exist = db.session.query(Users, Creds).join(Creds).filter(Users.email == emailid, Creds.otp == otp).order_by(Users.timestamp.desc()).limit(1)
-            print(len(data_exist))
-            for x in data_exist:
-                print(x.users_uid, x.users_email, x.creds_otp)
-            # if not data_exist:
-            #     return {'Status': "Invalid Emailid"}
-            # else:
-            #     if data_exist.otp == otp:
-            #         return {'Status': "Success"}
-            #     else:
-            #         return {'Status': "Invalid OTP"}
+            data = db.session.query(Users, Creds).join(Creds).filter(Users.email == emailid, Creds.otp == otp).one_or_none()
+            if not data.Creds:
+                return {'Status': "Invalid OTP"}
+            else:
+                return {'Status': "Success"}
         except Exception as e:
             return {'Status': "Error - " + str(e)}
